9d869d2fbe15e21eff9017aabe6209280262484b
alam-nazmul <alam.mdnazmul@outlook.com>
2024-02-29 02:01:35 +0600

    variable scopes

---
+++ variable_scopes.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	city := "Dhaka"
+	{
+		country := "Bangladesh"
+		fmt.Println(city)
+		fmt.Println(country)
+	}
+	// fmt.Println(country)
+	fmt.Println(city)
+}
