cf8cd0d359dd80be708cb30ad1b469611817b803
nazmul.alam@cellosco.pe <nazmul.alam@cellosco.pe>
2024-04-12 01:34:25 +0600

    single input

--- single_input.go
+++ single_input.go
@@ -2,9 +2,18 @@ package main
 
 import "fmt"
 
+<<<<<<< HEAD
 func main()  {
 	var name string
 	fmt.Println("Enter your name: ")
 	fmt.Scanf("%s", &name)
 	fmt.Println("Hi", name)
 }
+=======
+func main() {
+	var name string
+	fmt.Printf("Enter you name: ")
+	fmt.Scanf("%s", &name)
+	fmt.Println("Hi ", name)
+}
+>>>>>>> 3152205 (single input)
